9eba5742e6ce9211d43b3cabbba11fc3db9fc84c
suberb <suberb.163.com>
2018-05-01 23:06:47 +0800

    build

--- mfp-service/src/main/bin/build.sh
+++ mfp-service/src/main/bin/build.sh
@@ -1,2 +1,2 @@
-docker build  -t  192.168.1.109:5000/wyc-app-web:first  .
-docker push 192.168.1.109:5000/wyc-app-web:first
+sudo docker build  -t  192.168.1.109:5000/wyc-app-web:first  .
+sudo docker push 192.168.1.109:5000/wyc-app-web:first
